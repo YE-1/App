5473cfd89594e79b72a40e09a95479fe2235bbde
myw0410 <s19025@gsm.hs.kr>
2020-05-24 22:01:57 +0900

    ex01_practice10-1

---
+++ Chapter10/ex01_practice10-1/src/main/java/com/example/ex01_practice10_1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ex01_practice10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnNewActivity = findViewById(R.id.btnNewActivity);
+
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // SecondActivity의 정보를 담고있는 인텐트 객체 생성
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);  // SecondActivity 시작
+
+            }
+        });
+    }
+}
